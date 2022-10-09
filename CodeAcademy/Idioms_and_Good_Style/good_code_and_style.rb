48644fe329b8d5b6b4af98014504df82fc539a1d
Kirill <k.stulov@inbox.ru>
2022-10-10 00:17:30 +0500

    from laptop

---
+++ CodeAcademy/Idioms_and_Good_Style/good_code_and_style.rb
@@ -0,0 +1,19 @@
+# Занятие №8. Идиомы в Ruby и хороший стиль (46:50)
+# ХОРОШИЙ КОД И СТИЛЬ
+
+# Хороший код: 
+# Легко читаем
+# Прост (Принцип KISS keep it simple stupid)
+# Нет дублирования (принцип DRY)
+# Следует соглашениям о стиле
+  # Ruby Style Guide (49:10) -> https://github.com/rubocop/ruby-style-guide
+                              # -> https://github.com/arbox/ruby-style-guide/blob/master/README-ruRU.md  (перевод на русс)
+  # руководство по стилю написания программ на Ruby
+  # сбор правил, советов, методик и идиом, которые ведут к хорошему коду 
+
+  # RuboCop - стилистический анализатор кода (1:05:20)
+    # help you automatically check Ruby code against this guide.
+    # rubocop installation and usage (1:06:35)
+  # Если при установке RuboCop ругается на сертификат, нужно обновить сертификат GlobalSignRootCA.pem в папке c:\RailsInstaller\Ruby2.3.3\lib\ruby\2.3.0\rubygems\ssl_certs\
+  # Скачать его можно тут https://raw.githubusercontent.com/rubygems/rubygems/master/lib/rubygems/ssl_certs/rubygems.org/GlobalSignRootCA_R3.pem
+  # Инструкция тут: https://bundler.io/guides/rubygems_tls_ssl_troubleshooting_guide.html#updating-rubygems
